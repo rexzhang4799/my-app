68343e1b1089b98766dcb717327c0643e1804b91
rexzhang4799 <57111563@qq.com>
2021-09-01 10:54:45 +0800

    this no1

---
+++ src/App.js
@@ -0,0 +1,135 @@
+//import React from 'react';
+import React, { useState } from 'react';
+import { Button, DatePicker, Space, Table, Tag} from 'antd';
+import './App.css';
+import 'antd/dist/antd.css';
+//import { render } from 'react-dom';
+
+const App = () => {
+  // 声明一个新的叫做 “count” 的 state 变量
+  const [count, setCount] = useState(0);
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown1',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+      link:'www.qq.com',
+    },
+    {
+      key: '2',
+      name: 'Jim Green2',
+      age: 42,
+      address: 'London No. 2 Lake Park',
+      tags: ['loser'],
+      link:'xw.qq.com',
+    },
+    {
+      key: '3',
+      name: 'Joe Black3333',
+      age: 32,
+      address: 'Sidney No. 3 Lake Park',
+      tags: ['cool', 'teacher'],
+      link:'1.qq.com',
+    },
+    {
+      key: '4',
+      name: 'tom3',
+      age: 32,
+      address: 'Sidney No. 3 Lake Park',
+      tags: ['cool', 'teacher'],
+      link:'2.qq.com',
+    },
+    {
+      key: '5',
+      name: 'jerry3',
+      age: 32,
+      address: 'Sidney No. 3 Lake Park',
+      tags: ['cool', 'teacher'],
+      link:'3.qq.com',
+    },
+  ];
+  const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a href="www.qq.com">{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: tags => (
+      <>
+        {tags.map(tag => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'yellow';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <Space size="middle">
+        <a href="xx">Invite {record.name}</a>
+        <a href="xxx">Delete</a>
+      </Space>
+    ),
+  },
+  ];
+  //const arr = ['1文字文字文字','2文字文字文字','33文字文字文字','4文字文字文字','35文字文字文字','36文字文字文字','37文字文字文字']
+  const lists = data.map(porps=> 
+    <li key={porps.key}><a href={porps.link}>{porps.name + ' ' + porps.address}</a></li>
+    
+  )
+  class Weather extends React.Component{
+    render(){
+      return <div>今天天气不错</div>
+    }
+  }
+
+  return(
+    <>
+      <Space>
+        <Button type="primary">PRESS button</Button>
+        <Button type="dashed">dashed button</Button>
+        <Button type="text">text button</Button>
+        <Button type="link">link button</Button>
+        <DatePicker placeholder="select date" />
+      </Space>
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 10)}>
+          Click me
+        </button>
+      </div>
+      <Table columns={columns} dataSource={data} />
+      <ul>{lists}</ul>
+      <Weather/>
+    </>
+)
+
+}
+
+export default App;
