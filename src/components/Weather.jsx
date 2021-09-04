351d7d31b096c1d72ce296dda6570981b8c27c7c
rexzhang4799 <57111563@qq.com>
2021-09-04 21:53:48 +0800

    modif weather

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Hello from './Weather.module.css';
-export default class Weather extends Component {
+/* class Weather extends Component {
     render() {
         return (
-            <span className={Hello.title}>
+            <span className={ergou.title}>
                 类组件里面有this，有state，有生命周期
             </span>
         )
     }
+} */
+const Weather = () => {
+    return(
+        <span className={Hello.title}>
+                函数组件里面没有this，没有state，没有生命周期，以后可以用hooks实现！
+        </span>
+    )
 }
+
+
+export default Weather;
